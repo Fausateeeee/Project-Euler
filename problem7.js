1f3d3bb322d9fda410450119ef98d4ceffd0e099
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-09-06 12:59:50 -0400

    Problem 7 completed

---
+++ problem7.js
@@ -0,0 +1,55 @@
+/*jshint esversion: 6 */
+/*
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter a natural number ', (answer) => {
+
+    const parsed = parseInt(answer);
+
+    if (isNaN(parsed)) {
+        console.log("Please, enter a natural number next time <3");
+        return 0;
+    }
+
+    if (parsed < 0) {
+        return 0;
+    }
+
+    let primes = getPrimesArray(parsed);
+
+    console.log("The ", parsed, "-th prime is :", primes[primes.length - 1]);
+    rl.close();
+});
+
+function getPrimesArray(number)
+{
+    let primes = [2];
+    for (let i = 3; primes.length < number; i = i + 2) 
+    {
+        for (let j = 0; j < primes.length; j++)
+        {
+            if (i%primes[j] == 0)
+            {
+                break;
+            }
+            else if (j == primes.length - 1)
+            {
+                primes.push(i);
+            }
+        }
+    }
+
+    return primes;
+}
