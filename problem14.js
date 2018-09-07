aab3e71b5d5d9a2fc60b1959feb4a5077bcfca7b
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-09-07 17:09:04 -0400

    Start of problem 14

---
+++ problem14.js
@@ -0,0 +1,43 @@
+/*jshint esversion: 6 */
+/*
+
+The following iterative sequence is defined for the set of positive integers:
+
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter a natural number ', (answer) => {
+  
+    const parsed = parseInt(answer);
+
+    if (isNaN(parsed)) {
+        console.log("Please, enter a natural number next time <3");
+        rl.close();
+    }
+
+    if (parsed < 1) {
+        console.log("Enter a number greater than 0 next time");
+        rl.close();
+    }
+
+    console.log("triangular");
+    rl.close();
+});
