71d8a88d47128394c2051ae48382004b2edfdd37
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-09-10 15:43:33 -0400

    Start of problem 16

---
+++ problem16.js
@@ -0,0 +1,34 @@
+/*jshint esversion: 6 */
+/*
+
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter a natural number ', (answer) => {
+
+    const parsed = parseInt(answer);
+
+    if (isNaN(parsed)) {
+        console.log("Please, enter a natural number next time <3");
+        rl.close();
+    }
+
+    if (parsed < 1) {
+        console.log("Enter a number greater than 0 next time");
+        rl.close();
+
+    }
+
+    console.log("The sum of the digits of 2 to the power of", parsed, "is :", "BLANK");
+    rl.close();
+});
