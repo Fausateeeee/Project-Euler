060c2414e0f1b9fb303ec5e8f0cc565238fbd375
Charles-David Lachance <charlesd.lach@gmail.com>
2018-09-09 22:33:08 -0400

    Start of problem 15

---
+++ problem15.js
@@ -0,0 +1,34 @@
+/*jshint esversion: 6 */
+/*
+
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+How many such routes are there through a 20×20 grid?
+
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter a natural number ', (answer) => {
+  
+    const parsed = parseInt(answer);
+
+    if (isNaN(parsed)) {
+        console.log("Please, enter a natural number next time <3");
+        rl.close();
+    }
+
+    if (parsed < 1) {
+        console.log("Enter a number greater than 0 next time");
+        rl.close();
+    
+    }
+
+    console.log("The number of path in a", parsed,"x",parsed, "grid is :");
+    rl.close();
+});
