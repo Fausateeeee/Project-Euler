c5da08a0ea105bd05f4bdeb66aad7ef269d948d9
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-09-11 14:21:51 -0400

    Problem 16 completed, gitignore modifier to ignore npm modules

--- problem16.js
+++ problem16.js
@@ -7,6 +7,12 @@ What is the sum of the digits of the number 2^1000?
 
 */
 
+
+/*
+    I will need an implementation of BigInt
+    Run npm install big-integer 
+*/
+const bigInt = require("big-integer");
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -29,6 +35,8 @@ rl.question('Enter a natural number ', (answer) => {
 
     }
 
-    console.log("The sum of the digits of 2 to the power of", parsed, "is :", "BLANK");
+    let bi = bigInt(2).pow(parsed).toArray(10);
+
+    console.log("The sum of the digits of 2 to the power of", parsed, "is :", bi.value.reduce((a,b) => a+b));
     rl.close();
 });
