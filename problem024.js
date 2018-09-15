d5e10d454571d605a521efbbd65a857a31bd66cb
Charles-David Lachance <charlesd.lach@gmail.com>
2018-09-15 15:55:26 -0400

    Problem 24 completed

--- problem024.js
+++ problem024.js
@@ -32,6 +32,39 @@ rl.question('Write a natural number ', (answer) => {
         rl.close();
     }
 
-    console.log("The", parsed, "lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9 is", "ANSWER");
+    //let digits = ["0", "1", "2"];
+    let digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    let allPermuations = [];
+
+    computePermutation(digits, allPermuations, 0, digits.length - 1);
+
+    allPermuations.sort();
+    
+    console.log("The", parsed, "lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9 is", allPermuations[parsed - 1]);
     rl.close();
 });
+
+function computePermutation(arr, permutations, startingIndex, endIndex)
+{
+    if (startingIndex == endIndex)
+    {
+        permutations.push(arr.reduce((a,b) => {return a+b;}));
+    }
+    else
+    {
+        for (let i = startingIndex; i <= endIndex; i++)
+        {
+            swapArrayElements(arr, startingIndex, i);
+            computePermutation(arr, permutations, startingIndex + 1, endIndex);
+            swapArrayElements(arr, startingIndex, i);
+        }
+    }
+}
+
+function swapArrayElements(arr, index1, index2)
+{
+  let temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;  
+}
