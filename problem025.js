079eda9b21c26f17d56ad0c4e495ccfad508ac0a
Charles-David Lachance <charlesd.lach@gmail.com>
2018-09-15 15:58:34 -0400

    Start of problem 25

---
+++ problem025.js
@@ -0,0 +1,52 @@
+/*jshint esversion: 6 */
+/*
+
+The Fibonacci sequence is defined by the recurrence relation:
+
+    Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+
+Hence the first 12 terms will be:
+
+    F1 = 1
+    F2 = 1
+    F3 = 2
+    F4 = 3
+    F5 = 5
+    F6 = 8
+    F7 = 13
+    F8 = 21
+    F9 = 34
+    F10 = 55
+    F11 = 89
+    F12 = 144
+
+The 12th term, F12, is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter a natural number ', (answer) => {
+
+    const parsed = parseInt(answer);
+
+    if (isNaN(parsed)) {
+        console.log("Please, enter a natural number next time <3");
+        rl.close();
+    }
+
+    if (parsed < 3) {
+        console.log("Enter a number greater than 2 next time");
+        rl.close();
+    }
+
+    console.log("The first Fibonacci number to contain", parsed, " digits is :", "ANSWER");
+    rl.close();
+});
