f2c554d1f7d013ac68b87992d370c89d43b6fee4
Charles-David Lachance <charlesd.lach@gmail.com>
2018-09-15 16:47:28 -0400

    Problem 25 completed

--- problem025.js
+++ problem025.js
@@ -27,6 +27,7 @@ What is the index of the first term in the Fibonacci sequence to contain 1000 di
 */
 
 const readline = require('readline');
+const bigInt = require("big-integer");
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -42,11 +43,38 @@ rl.question('Enter a natural number ', (answer) => {
         rl.close();
     }
 
-    if (parsed < 3) {
-        console.log("Enter a number greater than 2 next time");
+    if (parsed < 1) {
+        console.log("Enter a number greater than 0 next time");
         rl.close();
     }
 
-    console.log("The first Fibonacci number to contain", parsed, " digits is :", "ANSWER");
+    if (parsed == 1)
+    {
+        console.log("The first Fibonacci number to contain", parsed, " digits is : 1");
+        rl.close();
+    }
+
+    let BigArray = [bigInt(1), bigInt(1)];
+    while (BigArray[BigArray.length - 1].toString(10).length < parsed)
+    {
+        let i = BigArray.length - 1;
+        BigArray.push(BigArray[i - 1].add(BigArray[i]));
+    }
+
+    console.log("The index of first Fibonacci number to contain", parsed, " digits is :", BigArray.length);
     rl.close();
 });
+
+function Fibonacci(F1, F2, length)
+{
+    let F3 = F1 + F2;
+    console.log(F3);
+    if(F3.toString().length < length)
+    {
+        return Fibonacci(F2, F3, length);
+    }
+    else
+    {
+        return F3;
+    }
+}
