cc1424811abe7249f9a9849a0d5287bbc8e48956
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-09-20 12:35:08 -0400

    Problem 28 completed

--- problem28.js
+++ problem28.js
@@ -3,11 +3,11 @@
 
 Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
 
-*21 22  23  24   *25
- 20 *7   8  *9   10
- 19  6  *1   2   11
- 18 *5  *4   3   12
-*17 16  15  14  *13
+*21  22  23  24 *25
+ 20  *7   8  *9  10
+ 19   6  *1   2  11
+ 18  *5  *4  *3  12
+*17  16  15  14 *13
 
 It can be verified that the sum of the numbers on the diagonals is 101.
 
@@ -31,7 +31,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Enter a natural number ', (answer) => {
+rl.question('Enter an odd natural number ', (answer) => {
 
     const parsed = parseInt(answer);
 
@@ -40,12 +40,39 @@ rl.question('Enter a natural number ', (answer) => {
         rl.close();
     }
 
-    if (parsed < 1) {
+    else if (parsed < 1) {
         console.log("Enter a number greater than 0 next time");
         rl.close();
     }
-    
 
-    console.log("The sum of the number on the diagnal of a", answer, "by", answer, "grid is :", "ANSWER");
-    rl.close();
+    else if (parsed%2 == 0)
+    {
+        console.log("Enter an odd number next time *o*");
+        rl.close();  
+    }
+    
+    else if (parsed == 1)
+    {
+        console.log("The sum of the number on the diagnal of a", 1, "by", 1, "grid is :", 1);
+        rl.close();
+    }
+
+    else
+    {
+        let diagonalTotal = 1;
+        let base = 1;
+
+        for (let rank = 3; rank <= parsed; rank += 2)
+        {
+            for (let j = 1; j <= 4; j++)
+            {
+                diagonalTotal += base + (rank - 1)*j;
+            }
+            base += (rank - 1)*4;
+        }
+
+        console.log("The sum of the number on the diagnal of a", answer, "by", answer, "grid is :", diagonalTotal);
+        rl.close();
+    }
+
 });
