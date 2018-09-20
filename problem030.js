f11a9f1d7d6c399b82093a9133ab84933fcf0ec6
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-09-20 13:00:02 -0400

    Start of problem 30

---
+++ problem030.js
@@ -0,0 +1,47 @@
+/*jshint esversion: 6 */
+/*
+
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+    1634 = 1^4 + 6^4 + 3^4 + 4^4
+    8208 = 8^4 + 2^4 + 0^4 + 8^4
+    9474 = 9^4 + 4^4 + 7^4 + 4^4
+
+As 1 = 1^4 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+*/
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter an natural number : ', (answer) => {
+
+    const parsed = parseInt(answer);
+
+    if (isNaN(parsed)) {
+        console.log("Please, enter a natural number next time <3");
+        rl.close();
+    }
+
+    else if (parsed <= 1) {
+        console.log("Enter a number greater than 1 next time");
+        rl.close();
+    }
+
+    else
+    {
+        console.log("There are", "NUMBER OF ANSWER", "numbers that can be written as the sum of the", parsed, 
+        "power of their digits. Their sum is : ", "ANSWER");
+        rl.close();
+    }
+
+});
