bab1c1b2d72c190ecb6c5fd8dd4699d6f3ada76a
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-10-03 12:44:33 -0400

    Problem 34 completed

--- problem034.js
+++ problem034.js
@@ -9,6 +9,10 @@ Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 
 */
 
+/*
+    A simble upper bound is that 9!*7 = 2540160, so a number can't equal the sum of their factorial over that
+*/
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -17,8 +21,25 @@ const rl = readline.createInterface({
 });
 
 rl.question('Press enter to continue : ', (answer) => {
-    
-    console.log("The sum of all numbers which are equal to the sum of the factorial of their digits is :", "ANSWER");
+
+    const factorial = { "0" : 1, "1" : 1, "2" : 2, "3" : 6, "4" : 24, "5" :120,
+                        "6" : 720, "7" : 5040, "8" : 40320, "9" : 362880};
+    let sol = 0;
+    for (let i = 10; i < 2540160; i++)
+    {
+        let num = i.toString().split("");
+        let sum = 0;
+        for (let j of num)
+        {
+            sum += factorial[j];
+        }
+        if (sum == i)
+        {
+            sol += i;
+        }
+    }
+    console.log("The sum of all numbers which are equal to the sum of the factorial of their digits is :", sol);
     rl.close();
 
 });
+
