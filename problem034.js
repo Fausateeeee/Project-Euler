8335f7c3d4c582d72196337e2ac810b54653c99f
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-10-03 12:30:12 -0400

    Start of problem 34

---
+++ problem034.js
@@ -0,0 +1,24 @@
+/*jshint esversion: 6 */
+/*
+
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Press enter to continue : ', (answer) => {
+    
+    console.log("The sum of all numbers which are equal to the sum of the factorial of their digits is :", "ANSWER");
+    rl.close();
+
+});
