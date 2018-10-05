2018abe3085086a47d5adf1c40bc8f4063aff241
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-10-05 18:32:23 -0400

    Start of problem 36

---
+++ problem036.js
@@ -0,0 +1,41 @@
+/*jshint esversion: 6 */
+/*
+
+The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+(Please note that the palindromic number, in either base, may not include leading zeros.)
+
+
+*/
+
+const readline = require('readline');
+const bigInt = require("big-integer");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter an natural number : ', (answer) => {
+
+    const parsed = parseInt(answer);
+
+    if (isNaN(parsed)) {
+        console.log("Please, enter a natural number next time <3");
+        rl.close();
+    }
+
+    else if (parsed < 1) {
+        console.log("Enter a number greater than 0 next time");
+        rl.close();
+    }
+
+    else
+    {
+        console.log("The sum of all palindromic number in base 10 and base 2 bellow", parsed, "is", "ANSWER");
+        rl.close();
+    }
+
+});
