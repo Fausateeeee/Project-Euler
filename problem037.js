b12da89c33ad697af2512f3beb499253564945dc
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-10-11 13:24:41 -0400

    Problem 37 completed

--- problem037.js
+++ problem037.js
@@ -48,9 +48,10 @@ rl.question('Enter an natural number between 1 and 11 : ', (answer) => {
     else
     {
 
-        console.log(truncablePrimes(parsed));
+        let arr = truncablePrimes(parsed);
 
-        console.log("The", parsed, "first truncables primes are", "ARR ANSWER", "and their sum is", "ANSWER");
+        console.log("The", parsed, "first truncables primes are", arr, "and their sum is", 
+        arr.reduce((a,b) => {return parseInt(a) + parseInt(b);}));
         rl.close();
     }
 
@@ -64,46 +65,91 @@ function truncablePrimes(bound)
     const firstPositionDigits = ["2","3","5","7"];
     const lastPositionDigits = ["3","7"];
     const middlePositionDigits = ["1","3","7","9"];
+    let upperBound = 1;
 
     while (truncable.length < bound)
     {
-        let upperBound = 1;
-
-        if (upperBound == 1)
+        
+        let positionMarker = [];
+        for (let i = 0; i <= upperBound; i++)
         {
-            for (let fpd of firstPositionDigits)
-            {
-
-                for (let lpd of lastPositionDigits)
-                {
-                    if (bigInt(parseInt(fpd + lpd)).isPrime())
-                    {
-                        truncable.push(fpd+lpd);
-                    }
-                }
-
-            }
+            positionMarker.push(0);
         }
-        else
+
+        while(positionMarker[0] < 4 )
         {
-            for (let fpd of firstPositionDigits)
+            let lastIndex = positionMarker.length - 1;
+            let firstPart = firstPositionDigits[positionMarker[0]];
+            let lastPart = lastPositionDigits[positionMarker[lastIndex]];
+            let middlePart = "";
+
+            for (let i = 1; i < upperBound; i++)
             {
-                for (let i = 0; i < upperBound - 1; i++)
-                {
-
-                }
-
-                for (let lpd of lastPositionDigits)
-                {
-                    if (bigInt(parseInt(fpd + lpd)).isPrime())
-                    {
-                        truncable.push(fpd+lpd);
-                    }
-                }
-
+                middlePart += middlePositionDigits[positionMarker[i]];
             }
+
+            if (isTruncablePrime(firstPart + middlePart + lastPart))
+            {
+                truncable.push(firstPart + middlePart + lastPart);
+            }
+
+            AdjustPositionMarker(positionMarker, lastIndex);
+
         }
+
+        upperBound++;
 
     }
-    return truncable;
+    return truncable.slice(0, bound);
+}
+
+function AdjustPositionMarker(positionMarker, lastIndex)
+{
+    //Adjust last index first since there are only 2 case to check
+    positionMarker[lastIndex]++;
+    if (positionMarker[lastIndex] > 1)
+    {
+        positionMarker[lastIndex] = 0;
+        positionMarker[lastIndex - 1]++;
+    }
+
+    //Adjust the middle position markers
+    for (let i = lastIndex - 1; i > 0; i--)
+    {
+        if (positionMarker[i] > 3)
+        {
+            positionMarker[i] = 0;
+            positionMarker[i - 1]++;
+        }
+    }
+}
+
+function isTruncablePrime(number)
+{
+    if (number.length == 2)
+    {
+        return bigInt(parseInt(number)).isPrime();
+    }
+    else
+    {
+        //left to right truncation
+        for (let i = 0; i < number.length - 1; i++)
+        {
+            if (!bigInt(parseInt(number.substring(i))).isPrime())
+            {
+                return false;
+            }
+        }
+
+        //right to left truncation
+        for (let i = number.length - 1; i > 0; i--)
+        {
+            if (!bigInt(parseInt(number.substring(0, i))).isPrime())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
