0e1b0ed903a7804b0eadd37f92609f5901bf99e0
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-10-11 14:39:04 -0400

    Problem 38 completed

--- problem038.js
+++ problem038.js
@@ -18,6 +18,15 @@ of an integer with (1,2, ... , n) where n > 1?
 
 */
 
+/*
+    The fact that n must be greater than 1 limits the number of possibility we have to check:
+
+    If n is 2, the number can't be greater than 9876 and must be greater than 2500
+    If n is 3, the number can't be greater than 321 and it must be greater than 100
+    If n is 4, the number can't be greater than 33 and it must be greater than 25
+    If n is 5, the number can't be greater than 9 and it must be greater than 2
+*/
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -28,8 +37,87 @@ const rl = readline.createInterface({
 rl.question('Press enter to continue', (answer) => {
 
     console.log("The largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer is",
-    "ANSWER");
+    getPandigital());
     rl.close();
 
 
 });
+
+function getPandigital()
+{
+    let solution = [];
+    for (let nbr = 2500; nbr <= 9876; nbr++)
+    {
+        if (isPandigital(nbr, nbr*2))
+        {
+            solution.push([(nbr).toString(), (nbr*2).toString()]);
+        }
+    }
+    for (let nbr = 100; nbr <= 321; nbr++)
+    {
+        if (isPandigital(nbr, nbr*2, nbr*3))
+        {
+            solution.push([nbr.toString(), (nbr*2).toString(), (nbr*3).toString()]);
+        }
+    }
+    for (let nbr = 25; nbr <= 33; nbr++)
+    {
+        if (isPandigital(nbr, nbr*2, nbr*3, nbr*4))
+        {
+            solution.push([nbr.toString(), (nbr*2).toString(), (nbr*3).toString(), (nbr*4).toString()]);
+        }
+    }
+    for (let nbr = 2; nbr <= 9; nbr++)
+    {
+        if (isPandigital(nbr, nbr*2, nbr*3, nbr*4, nbr*5))
+        {
+            solution.push([nbr.toString(), (nbr*2).toString(), (nbr*3).toString(), (nbr*4).toString(), (nbr*5).toString()]);
+        }
+    }
+
+    return getMaxPandigital(solution);
+}
+
+function isPandigital()
+{
+    let nbrs ="x";
+    for (let arg of arguments)
+    {
+        nbrs += arg;
+    }
+    nbrs += "x";
+
+    if (nbrs.indexOf("0") != -1)
+    {
+        return false;
+    }
+    for (let i = 1; i <= 9; i++)
+    {
+        let currentSplit = nbrs.split(i.toString()).length;
+
+        if(currentSplit != 2)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+function getMaxPandigital(arr)
+{
+    let max = 0;
+    for (let sol of arr)
+    {
+        let possibleSol = "";
+        for (let nbr of sol)
+        {
+            possibleSol += nbr;
+        }
+
+        if (max < parseInt(possibleSol))
+        {
+            max = parseInt(possibleSol);
+        }
+    }
+    return max;
+}
