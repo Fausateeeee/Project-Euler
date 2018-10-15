3e1e75f117a528cca7e2c9d7347e768594461006
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-10-15 18:03:57 -0400

    Problem 41 completed, pure brute force and quite slow

--- problem041.js
+++ problem041.js
@@ -8,17 +8,18 @@ What is the largest n-digit pandigital prime that exists?
 
 */
 /*
-    I can probably brute force it but there is probably an easy solution
+
 */
 
 const readline = require('readline');
+const bigInt = require("big-integer");
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('Enter a natural number for 10^n : ', (answer) => {
+rl.question('Enter a natural number : ', (answer) => {
 
     const parsed = parseInt(answer);
 
@@ -34,9 +35,49 @@ rl.question('Enter a natural number for 10^n : ', (answer) => {
 
     else
     {
-        console.log("The greatest prime that is the closest to 1 to", parsed, 
-        "pandigital is", "ANSWER");
+        //console.log(2143, 2143%3, 2143%5, isPandigital(2143, 4), bigInt(2143).isPrime());
+        for (let i = Math.pow(10,parsed) - 1; i >= 1; i = i - 2)
+        {
+            //console.log(i);
+            if (i%3 != 0 && i%5 != 0 && isPandigital(i, i.toString().length) && bigInt(i).isPrime())
+            {
+                console.log("The greatest prime that is the closest to 1 to", parsed, 
+                "pandigital is", i);
+                break;
+            }
+        }
+
         rl.close();
     }
 
 });
+
+function isPandigital(number,length)
+{
+    let nbrs ="x" + number.toString() + "x";
+
+    if (nbrs.indexOf("0") != -1)
+    {
+        return false;
+    }
+
+    for (let i = length + 1; i < 9; i++)
+    {
+        if (nbrs.indexOf(i.toString()) != -1)
+        {
+            return false;
+        }
+    }
+
+    for (let i = 1; i <= length; i++)
+    {
+
+        let currentSplit = nbrs.split(i.toString()).length;
+
+        if(currentSplit != 2)
+        {
+            return false;
+        }
+    }
+    return true;
+}
