f4f651ae75f9896dd2d4117ba14f09067ffe98be
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-10-18 13:34:36 -0400

    Start of problem 46

---
+++ problem046.js
@@ -0,0 +1,32 @@
+/*jshint esversion: 6 */
+/*
+
+It was proposed by Christian Goldbach that every odd composite number can be written as the sum of a prime and twice a square.
+
+9 = 7 + 2×1^2
+15 = 7 + 2×2^2
+21 = 3 + 2×3^2
+25 = 7 + 2×3^2
+27 = 19 + 2×2^2
+33 = 31 + 2×1^2
+
+It turns out that the conjecture was false.
+
+What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
+
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Press enter to continue : ', (answer) => {
+
+    console.log("The smallest odd composite that cannot be written as the sum of a prime and twice a square is", "ANSWER");
+    rl.close();
+
+
+});
