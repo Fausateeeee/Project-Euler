a8bf2e4d9cd9e7318d408fcd3a868ae891528fe3
Charles-David Lachance <charles-david.lachance@canada.ca>
2018-12-12 14:47:09 -0500

    Finally completed p54, the sort used was sorting as if int were strings

--- problem054.js
+++ problem054.js
@@ -61,7 +61,7 @@ const rl = readline.createInterface({
 });
 
 rl.question('Write the path to a valid input file  : ', (answer) => {
-    let allText = fs.readFileSync("/home/fausateeeee/Projects/Project-Euler/Additional-Files/p054_poker.txt", 'utf8');
+    let allText = fs.readFileSync("C:\\Projects\\P\\Project-Euler\\Additional-Files\\p054_poker.txt", 'utf8');
     //Split the names into an array
     let arrAllHands = allText.split("\n");
     let P1winCount = 0;
@@ -70,7 +70,7 @@ rl.question('Write the path to a valid input file  : ', (answer) => {
         let player1 = hands.substring(0, 14).split(" ");
         let player2 = hands.substring(15).split(" ");
         P1winCount += ComparePokerHand(player1, player2);
-        console.log(player1, EvaluatePokerHands(player1), "|||", player2, EvaluatePokerHands(player2), "---", ComparePokerHand(player1, player2));
+        console.log(player1, player2, ComparePokerHand(player1, player2));
     }
     console.log("The player 1 wins exactly", P1winCount, "hands.");
     rl.close();
@@ -80,12 +80,206 @@ function ComparePokerHand(player1, player2)
 {
     let P1 = EvaluatePokerHands(player1);
     let P2 = EvaluatePokerHands(player2);
-    return P1[0] > P2[0] ? 1 : (P1[0] == P2[0] ? (P1[1] > P2[1] ? 1 : 0) : 0);
+
+    //If player 1 got a better hand, return a win for P1
+    if (P1[0] > P2[0])
+    {
+        return 1;
+    }
+    //If there is a tie in the power of the hand
+    else if (P1[0] == P2[0])
+    {
+        //Both got straight flush
+        if(P1[0] == 8)
+        {
+            if(P1[1][4] > P2[1][4])
+            {
+                return 1;
+            }
+            //This is a tie or a loss for P1
+            return 0;
+        }
+        //Both got four of a kind
+        if(P1[0] == 7)
+        {
+            //If the four of a kind of P1 is greater, return a win
+            if(P1[1] > P2[1])
+            {
+                return 1;
+            }
+            //Else this is a loss for P1
+            return 0;
+        }
+        //If both player got a full house
+        if(P1[0] == 6)
+        {
+            //If the three of a kind is greater than P2
+            if(P1[1] > P2[1])
+            {
+                return 1;
+            }
+            //Else this is a loss for P1
+            return 0;
+            
+        }
+        //If both players got a flush
+        if(P1[0] == 5)
+        {
+            //Compare from highest to lowest card
+            for(let i = 4; i >= 0; --i)
+            {
+                //P1 wins
+                if(P1[1][i] > P2[1][i])
+                {
+                    return 1;
+                }
+                //P1 loss
+                else if (P1[1][i] < P2[1][i])
+                {
+                    return 0;
+                }
+            }
+            //Tie, no win
+            return 0;
+        }
+        //If both players got a straight
+        if(P1[0] == 4)
+        {
+            //Compare highest card
+            //P1 wins
+            if(P1[1][4] > P2[1][4])
+            {
+                return 1;
+            }
+            //Tie or loss, no win
+            return 0;
+        }
+        //If both players got three of a kind
+        if(P1[0] == 3)
+        {
+            //Three of a kind of P1 is greater, P1 wins
+            if(P1[1] > P2[1])
+            {
+                return 1;
+            }
+            //Else this is a loss
+            return 0;
+        }
+        //If both players got two pairs
+        if(P1[0] == 2)
+        {
+            //P1 has the highest pair, win
+            if (P1[1] > P2[1])
+            {
+                return 1;
+            }
+            //P2 has the highest pair, loss
+            else if (P1[1] < P2[1])
+            {
+                return 0;
+            }
+                //Both have the same highest pair
+            else
+            {
+                //P1 has the second highest pair, win
+                if (P1[2] > P2[2])
+                {
+                    return 1;
+                }
+                //P2 has the second highest pair, loss
+                else if (P1[2] < P2[2])
+                {
+                    return 0;
+                }
+                //Both has the same two pairs, check the last card
+                else
+                {
+                    //Compare from highest to lowest card
+                    for(let i = 4; i >= 0; --i)
+                    {
+                        //P1 has the highest card
+                        if(P1[3][i] > P2[3][i])
+                        {
+                            return 1;
+                        }
+                        //P2 has the highest card
+                        else if (P1[3][i] < P2[3][i])
+                        {
+                            return 0;
+                        }
+                    }
+                }
+            }
+            //Tie, no win
+            return 0;
+        }
+        //Both players have a pair
+        if(P1[0] == 1)
+        {
+            //P1 has the highest pair, win
+            if (P1[1] > P2[1])
+            {
+                return 1;
+            }
+            //P2 has the highest pair, loss
+            else if (P1[1] < P2[1])
+            {
+                return 0;
+            }
+            //Both players have the same pair
+            else
+            {
+                //Compare from highest to lowest card
+                for(let i = 4; i >= 0; --i)
+                {
+                    //P1 has the highest card
+                    if(P1[2][i] > P2[2][i])
+                    {
+                        return 1;
+                    }
+                    //P2 has the highest card
+                    else if (P1[2][i] < P2[2][i])
+                    {
+                        return 0;
+                    }
+                }
+            }
+            //Tie, no win
+            return 0;
+        }
+        //Both players have nothing, compare highest to lowest
+        if(P1[0] == 0)
+        {
+            //Compare from highest to lowest card
+            for(let i = 4; i >= 0; --i)
+            {
+                //P1 has the highest card
+                if(P1[1][i] > P2[1][i])
+                {
+                    console.log(player1, "--", P1[1], "|||", player2, "--", P2[1], 1);
+                    return 1;
+                }
+                //P2 has the highest card
+                else if (P1[1][i] < P2[1][i])
+                {
+                    console.log(player1, "--", P1[1], "|||", player2, "--", P2[1], 0);
+                    return 0;
+                }
+            } 
+            //Tie, no win
+            return 0;
+        }
+
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 function EvaluatePokerHands(hand)
 {
-    H = {high : GetHighestCard(hand), 
+    H = {high : GetHighestCards(hand), 
           pair : HasOnePair(hand), 
           twopair : HasTwoPair(hand),
           threekind : HasThreeOfAKind(hand),
@@ -95,37 +289,37 @@ function EvaluatePokerHands(hand)
           fourkind : HasFourOfAKind(hand)         
           }; 
 
-    if (H.flush && H.straight)
+    if (H.flush[0] && H.straight[0])
     {
         return [8, H.high];
     }
-    else if (H.fourkind)
+    else if (H.fourkind[0])
     {
-        return [7, H.high];
+        return [7, H.fourkind[1], H.high];
     }
-    else if (H.fullhouse)
+    else if (H.fullhouse[0])
     {
-        return [6, H.high];
+        return [6, H.fullhouse[1], H.high];
     }
-    else if (H.flush)
+    else if (H.flush[0])
     {
         return [5, H.high];
     }
-    else if (H.straight)
+    else if (H.straight[0])
     {
         return [4, H.high];
     }
-    else if (H.threekind)
+    else if (H.threekind[0])
     {
-        return [3, H.high];
+        return [3, H.threekind[1], H.high];
     }
-    else if (H.twopair)
+    else if (H.twopair[0])
     {
-        return [2, H.high];
+        return [2, H.twopair[1], H.twopair[2], H.high];
     }
-    else if (H.pair)
+    else if (H.pair[0])
     {
-        return [1, H.high];
+        return [1, H.pair[1], H.high];
     }
     else
     {
@@ -142,7 +336,7 @@ function HasOnePair(hand)
         if (analysis[card[0]])
         {
 
-            return [true, card[0]];
+            return [true, ConvertCardToNumericValue(card[0])];
         }
         else
         {
@@ -164,7 +358,7 @@ function HasTwoPair(hand)
             {
                 let lowestPair;
                 let highestPair;
-                if(onePair[1] < card[0])
+                if(ConvertCardToNumericValue(onePair[1]) < ConvertCardToNumericValue(card[0]))
                 {
                     lowestPair = onePair[1]; 
                     highestPair = card[0];
@@ -174,7 +368,7 @@ function HasTwoPair(hand)
                     lowestPair = card[0]; 
                     highestPair = onePair[1];
                 }
-                return [true, highestPair, lowestPair];
+                return [true, ConvertCardToNumericValue(highestPair), ConvertCardToNumericValue(lowestPair)];
             }
             onePair = [true, card[0]];
         }
@@ -193,7 +387,7 @@ function HasThreeOfAKind(hand)
     {
         if (analysis[card[0]])
         {
-            return [true, card[0]];
+            return [true, ConvertCardToNumericValue(card[0])];
         }
         else
         {
@@ -217,7 +411,7 @@ function HasFourOfAKind(hand)
     {
         if (analysis[card[0]])
         {
-            return [true, card[0]];
+            return [true, ConvertCardToNumericValue(card[0])];
         }
         else
         {
@@ -243,37 +437,15 @@ function HasStraight(hand)
     let straight= [];
     for (let card of hand)
     {
-        let value = card[0];
-
-        switch(value)
-        {
-            case "A":
-                straight.push(14);
-                break;
-            case "K":
-                straight.push(13);
-                break;
-            case "Q":
-                straight.push(12);
-                break;
-            case "J":
-                straight.push(11);
-                break;
-            case "T":
-                straight.push(11);
-                break;
-            default:
-                straight.push(Number.parseInt(value));
-                break;           
-        }
+        straight.push(ConvertCardToNumericValue(card[0]));
     }
     straight.sort();
     if (straight[4] == straight[0] + 4 && straight[3] == straight[0] + 3 &&
         straight[2] == straight[0] + 2 && straight[1] == straight[0] + 1)
         {
-            return true;
+            return [true];
         }
-    return false;
+    return [false];
 }
 //Works
 function HasFlush(hand)
@@ -284,11 +456,11 @@ function HasFlush(hand)
     {
         if (suit != card[1])
         {
-            return false;
+            return [false];
         }
     }
 
-    return true;
+    return [true];
 }
 
 function HasFullHouse(hand)
@@ -315,7 +487,7 @@ function HasFullHouse(hand)
             {
                 if (analysis[key] == 2)
                 {
-                    pair = [true, key];
+                    pair = [true];
                 }
                 if (analysis[key] == 3)
                 {
@@ -324,7 +496,7 @@ function HasFullHouse(hand)
             }
             if (pair[0] && threeofakind[0])
             {
-                return [true, threeofakind[1], pair[1]];
+                return [true, ConvertCardToNumericValue(threeofakind[1])];
             }
         }
 
@@ -338,29 +510,36 @@ function GetHighestCards(hand)
 
     for (let card of hand)
     {
-        let value = card[0];
+        highest.push(ConvertCardToNumericValue(card[0]));
+    }
+    return highest.sort((a,b) => {
+        if (Number.parseInt(a) > Number.parseInt(b))
+        {
+            return 1;
+        }
+        else if (Number.parseInt(a) < Number.parseInt(b))
+        {
+            return -1;
+        }
+        return 0;       
+    });
+}
 
-        switch(value)
+function ConvertCardToNumericValue(card)
+{
+    switch(card)
         {
             case "A":
-                highest.push(14);
-                break;
+                return 14;
             case "K":
-                highest.push(13);
-                break;
+                return 13;
             case "Q":
-                highest.push(12);
-                break;
+                return 12;
             case "J":
-                highest.push(11);
-                break;
+                return 11;
             case "T":
-                highest.push(10);
-                break;
+                return 10;
             default:
-                highest.push(value);
-                break;         
+                return Number.parseInt(card);  
         }
-    }
-    return highest.sort();
 }
