12bd997fce179e59f99b0da7f0cf95c35063400e
Charles-David Lachance <charles-david.lachance@canada.ca>
2019-03-06 19:12:50 -0500

    Solution to problem 60 finally found, not beautiful but ok!

--- problem060.js
+++ problem060.js
@@ -18,8 +18,8 @@ const rl = readline.createInterface({
 
 rl.question('Press enter to continue : ', (answer) => {
 
-    console.log("The lowest sum for a set of five primes for which any two primes concatenate to produce another prime is",
-    GenerateOddPrimeArray(50000));
+    console.log("The lowest sum for a set of five primes for which any two primes concatenate to produce another prime is", 
+    FindPrimeFamily(GenerateOddPrimeArray(10000)));
     rl.close();
 });
 
@@ -27,47 +27,62 @@ function FindPrimeFamily(primes)
 {
     for (let index1 = 0; index1 < primes.length; ++index1)
     {
-        let prime1 = primes[index1];
-        for (let index2 = index1 + 1; index2 < primes.length; ++index2)
+        let prime1Family = ComputePrimeFamily(index1, primes);
+        if (prime1Family.length > 0)
         {
-            let prime2 = primes[index2];
-            
-            for (let index3 = index2 + 1; index3 < primes.length; ++index3)
+            for (let index2 = 0; index2 < prime1Family.length; ++index2)
             {
-                for (let index4 = index3 + 1; index4 < primes.length; ++index4)
+                let prime2Family = ComputePrimeFamily(index2, prime1Family);
+                if (prime2Family.length > 0)
                 {
-                    for (let index5 = index4 + 1; index5 < primes.length; ++index5)
+                    for (let index3 = 0; index3 < prime2Family.length; ++index3)
                     {
-                        
+                        let prime3Family = ComputePrimeFamily(index3, prime2Family);
+                        if (prime3Family.length > 0)
+                        {
+                            for (let index4 = 0; index4 < prime3Family.length; ++index4)
+                            {
+                                let prime4Family = ComputePrimeFamily(index4, prime3Family);
+                                if (prime4Family.length > 0)
+                                {
+                                    return [primes[index1], prime1Family[index2], prime2Family[index3], 
+                                    prime3Family[index4], prime4Family[0]].reduce((a,b) => parseInt(a) + parseInt(b));
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
+
     }
+    return 0;
 }
 
-function CheckConcatenate(potentialPrime, startingFamily)
+function ComputePrimeFamily(index, primes)
 {
-    for (let prime of startingFamily)
+    let currentPrime = primes[index];
+    let primeFamily = [];
+    for (let i = index + 1; i < primes.length; i++)
     {
-        let prime1 = parseInt(prime.toString() + potentialPrime.toString());
-        let prime2 = parseInt(potentialPrime.toString() + prime.toString());
-        if (!bigInt(prime1).isPrime() || !bigInt(prime2).isPrime())
+        let testPrime1 = currentPrime + primes[i];
+        let testPrime2 = primes[i] + currentPrime;
+        if (bigInt(parseInt(testPrime1)).isPrime() && bigInt(parseInt(testPrime2)).isPrime())
         {
-            return false;
+            primeFamily.push(primes[i]);
         }
     }
-    return true;
+    return primeFamily;
 }
 
 function GenerateOddPrimeArray(upperbound)
 {
-    primes = [3,7,11];
+    let primes = ["3","7","11"];
     for (let p = 13; p < upperbound; p += 2)
     {
         if (p%3 != 0 && p%5 != 0 && p%7 != 0 && p%11 !=0 && bigInt(p).isPrime())
         {
-            primes.push(p);
+            primes.push(p.toString());
         }
     }
     return primes;
