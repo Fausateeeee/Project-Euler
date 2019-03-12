d890bd1f96c582349c9ef7b8b5bbbec4cf19e9e3
Charles-David Lachance <charles-david.lachance@canada.ca>
2019-03-12 18:40:20 -0400

    Problem completed

--- problem066.js
+++ problem066.js
@@ -22,16 +22,115 @@ Hence, by considering minimal solutions in x for D ≤ 7, the largest x is obtai
 Find the value of D ≤ 1000 in minimal solutions of x for which the largest value of x is obtained.
 
 */
-const readline = require('readline');
 
+/*
+
+We are trying to solve Pell's equation
+https://en.wikipedia.org/wiki/Pell%27s_equation#Solutions
+
+*/
+const readline = require('readline');
+const bigInt = require('big-integer');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+
 rl.question('Press enter to continue : ', (answer) => {
 
-    console.log("The lowest sum for a set of five primes for which any two primes concatenate to produce another prime is",
-    "ANSWER");
+    console.log("The value of D where x is the largest for D ≤ 1000 is", Diophantine(1000)[1]);
     rl.close();
 });
+
+function Diophantine(upperBound)
+{
+    let max = [bigInt(0),0];
+    for (let constant = 2; constant <= upperBound; ++constant)
+    {
+        if (Number.isInteger(Math.sqrt(constant)))
+        {
+            continue;
+        }
+
+        let period = ComputePeriod(constant);
+
+        let numerator = [bigInt(0),bigInt(1)];
+        let denominator = [bigInt(1),bigInt(0)];
+        ComputeConvergent(numerator, denominator, period.shift());
+        let periodIndex = 0;
+        while (true)
+        {          
+            let dio_answer = numerator[1].pow(2).minus(denominator[1].pow(2).times(constant));
+            if (dio_answer.equals(1))
+            {
+                if (max[0].lt(numerator[1]))
+                {
+                    max[0] = numerator[1];
+                    max[1] = constant;
+                }
+                break;
+            }
+            ComputeConvergent(numerator, denominator, period[periodIndex]);
+            periodIndex = (periodIndex + 1)%period.length;
+        }
+
+    }
+    return max;
+}
+
+function ComputePeriod(radical)
+{
+
+    let root = Math.sqrt(radical);
+    let int_part = Math.floor(root);
+    let period = [int_part];
+    let numerator = [1];
+    let denominator = [-1*int_part];
+
+    denominator.push((radical - Math.pow(denominator[0], 2))/numerator[0]);
+    period.push(Math.floor((root - denominator[0])/denominator[1]));
+    numerator.push(-1*denominator[0] - (period[1] * denominator[1]));
+    let has_looped = {};
+    has_looped[numerator[1]] = [denominator[1]];
+    _ComputePeriod(period, numerator, denominator, radical, root, has_looped);
+    period.pop();
+
+    return period;
+}
+
+function _ComputePeriod(period, numerator, denominator, radical, root, has_looped)
+{
+    while (true)
+    {
+        let index = numerator.length - 1;
+        denominator.push((radical - Math.pow(numerator[index], 2))/denominator[index]);
+        period.push(Math.floor((root - numerator[index])/denominator[index + 1]));
+        numerator.push((-1 * period[index + 1] * denominator[index + 1]) - numerator[index]);
+    
+        if (has_looped.hasOwnProperty([numerator[index + 1]]))
+        {
+            if (has_looped[numerator[index + 1]].indexOf(denominator[index + 1]) != -1)
+            {
+                return;
+            }
+            else
+            {
+                has_looped[numerator[index + 1]].push(denominator[index + 1]);
+            }
+        }
+        else
+        {
+            has_looped[numerator[index + 1]] = [denominator[index + 1]];
+        }
+    }
+
+}
+
+function ComputeConvergent(numerator, denominator, period)
+{
+    numerator.push(numerator[1].times(period).plus(numerator[0]));
+    denominator.push(denominator[1].times(period).plus(denominator[0]));
+    numerator.shift();
+    denominator.shift();
+}
