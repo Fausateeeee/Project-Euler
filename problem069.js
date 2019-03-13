7a70d4164049306e92486dd60abd56557515248c
Charles-David Lachance <charles-david.lachance@canada.ca>
2019-03-13 18:15:43 -0400

    Code is working but need optimisation since its too slow

--- problem069.js
+++ problem069.js
@@ -22,15 +22,95 @@ Find the value of n ≤ 1,000,000 for which n/φ(n) is a maximum.
 
 */
 const readline = require('readline');
-
+const bigInt = require('big-integer');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 rl.question('Press enter to continue : ', (answer) => {
-
     console.log("The lowest sum for a set of five primes for which any two primes concatenate to produce another prime is",
-    "ANSWER");
+    ComputeMaximumRatio(1000000));
     rl.close();
 });
+
+function ComputeMaximumRatio(upperbound)
+{
+    let primes = GeneratePrimeArray(upperbound);
+    let max = {number : 0, ratio : 0};
+    for (let i = 2; i <= upperbound; i++)
+    {
+        let totient = (EulerTotient(i, primes));
+        if (i/totient > max.ratio)
+        {
+            max.number = i;
+            max.ratio = i/totient;
+        }
+    }
+    return max.number;
+}
+
+function EulerTotient(number, primes)
+{
+    if (primes.indexOf(number) != -1)
+    {
+        return number - 1;
+    }
+    else
+    {
+        let totient = {};
+        for (let prime of primes)
+        {
+            while (number%prime == 0)
+            {
+                if (totient.hasOwnProperty(prime))
+                {
+                    totient[prime] += 1;
+                }
+                else
+                {
+                    totient[prime] = 1;
+                }
+                number /= prime;
+                if (number == 1)
+                {
+                    return _EulerTotient(totient);
+                }
+            }
+
+        }
+    }
+}
+
+function _EulerTotient(primeFactorisation)
+{
+    let totient = 1;
+    for (let key of Object.keys(primeFactorisation))
+    {
+        prime = parseInt(key);
+        if (primeFactorisation[key] == 1)
+        {
+            totient *= prime - 1;
+        }
+        else
+        {
+            totient *= Math.pow(prime,primeFactorisation[key] - 1)*(prime - 1);
+        }
+    }
+
+    return totient;
+}
+
+function GeneratePrimeArray(upperbound)
+{
+    let primes = [2, 3, 5, 7, 11];
+    for (let i = 13; i <= upperbound; i += 2)
+    {
+        if (i%3 != 0 && i%5 != 0 && i%7 != 0 && i%11 != 0 && bigInt(i).isPrime())
+        {
+            primes.push(i);
+        }
+    }
+
+    return primes;
+}
