97be48a185d2d26154218ca4297e9bcf58b29f4e
Charles-David Lachance <charles-david.lachance@canada.ca>
2019-05-15 18:18:00 -0400

    Problem completed

--- problem074.js
+++ problem074.js
@@ -33,9 +33,67 @@ const rl = readline.createInterface({
 });
 
 rl.question('Press enter to continue : ', (answer) => {
-
     console.log("There are",
-    "ANSWER",
+    ChainLoop(1000000),
     "chains containing exactly sixty non-repeating terms.");
     rl.close();
 });
+
+function ChainLoop(upperbound)
+{
+    let total = 0;
+    for(let i = 3; i< upperbound; ++i)
+    {
+        if(FindChainLength(i) == 60)
+        {
+            ++total;
+        }
+    }
+
+    return total;
+}
+
+function FindChainLength(number)
+{
+
+    let nombre = number;
+    let chain = [];
+    do{
+        chain.push(nombre);
+        arr = nombre.toString().split("");
+        nombre=0;
+        for(let nbr of arr)
+        {
+            nombre += Factorial(nbr);
+        }
+    }
+    while(!chain.includes(nombre));
+
+    return chain.length;
+
+}
+
+function Factorial(number)
+{
+    switch(number)
+    {
+        case '2':
+            return 2;
+        case '3':
+            return 6;
+        case '4':
+            return 24;
+        case '5':
+            return 120;
+        case '6':
+            return 720;
+        case '7':
+            return 5040;
+        case '8':
+            return 40320;
+        case '9':
+            return 362880;
+        default:
+            return 1;
+    }
+}
