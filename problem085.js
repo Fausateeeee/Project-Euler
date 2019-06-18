f738a5f87fe352a146fe34a18febda5156e12ce4
Charles-David Lachance <charles-david.lachance@canada.ca>
2019-06-18 16:25:43 -0400

    Problem completed

--- problem085.js
+++ problem085.js
@@ -19,9 +19,16 @@ find the area of the grid with the nearest solution.
     j x 1 = m(n - k + 1);
     1 x k = n(m - j + 1);
 
-    In fact, we have the more general equation
+    In fact, we have the more general equation for a specific sub-rectangle size
 
     j x k = (n - j + 1)(m - k + 1);
+
+    The total of sub-rectangle in a rectangle of size n x m is
+
+    sum_{k=1}^m sum_{j=1}^n (n - j + 1)(m - k + 1) =  sum_{k=1}^m (m - k + 1) sum_{j=1}^n (n - j + 1)
+                                                   = (m + (m-1) + (m-2) + ... + 1)(n + (n-1) + (n-2) + ... + 1)
+                                                   = (m(m+1)/2)(n(n+1)/2)
+                                                   = nm(n + 1)(m + 1)/4;
 
 */
 const readline = require('readline');
@@ -32,7 +39,48 @@ const rl = readline.createInterface({
 
 rl.question('Press enter to continue : ', (answer) => {
 
-    console.log("The first value which can be written as the sum of primes in over five thousand different ways is", 
-    "ANSWER");
+    console.log("The area of the grid with the nearest total of sub-rectangle to 2,000,000 is", 
+    FindClosestRectangle());
     rl.close();
 });
+
+function FindClosestRectangle()
+{
+    const TOTAL = 2000000;
+    let n = 1;
+    let m = 1;
+    let n_f = 1;
+    let m_f = 1;
+    let difference = 2000000;
+    let total = 1;
+    while (total < TOTAL)
+    {
+        total = TotalOfSubrectangle(n,m);
+        if (n%10 == 0)
+        {
+            console.log(n,m);
+        }
+        total = TotalOfSubrectangle(n,m);
+        if (Math.abs(TOTAL - total) < difference)
+        {
+            n_f = n;
+            m_f = m;
+            difference = Math.abs(TOTAL - total);
+        }
+        if (total > TOTAL)
+        {
+            m = 1;
+            ++n;   
+            total = TotalOfSubrectangle(n,m);       
+        }
+        ++m;
+        
+    }
+
+    return n_f * m_f;
+}
+
+function TotalOfSubrectangle(n,m)
+{
+    return  n*m*(n + 1)*(m + 1)/4;
+}
