9de3667cec2000c5953b17e488cab870483a2b26
Charles-David Lachance <charles-david.lachance@canada.ca>
2019-06-27 12:58:55 -0400

    Start of the problem

---
+++ problem094.js
@@ -0,0 +1,27 @@
+/*jshint esversion: 6 */
+/*
+
+    It is easily proved that no equilateral triangle exists with integral length sides 
+    and integral area. However, the almost equilateral triangle 5-5-6 has an area of 12 square units.
+
+    We shall define an almost equilateral triangle to be a triangle for 
+    which two sides are equal and the third differs by no more than one unit.
+
+    Find the sum of the perimeters of all almost equilateral triangles with integral 
+    side lengths and area and whose perimeters do not exceed one billion (1,000,000,000).
+
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Press enter to continue : ', (answer) => {
+
+    console.log("The sum of all the minimal product-sum numbers for 2 ≤ k ≤ 12000 is", 
+    "ANSWER");
+    rl.close();
+});
