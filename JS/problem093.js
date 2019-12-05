44c40f060b7c2e699270d564c593073a23524742
Charles-David Lachance <charles-david.lachance@canada.ca>
2019-12-05 17:46:49 -0500

    Problem completed

--- JS/problem093.js
+++ JS/problem093.js
@@ -25,6 +25,18 @@
 
 */
 
+/*
+    Omitting the parentheses, there are 4^3 = 64 combinations possible of arithmetic operations.
+    From 9 numbers, we choose 4 and have 126 sets to check.
+
+    The first thing to notice is that if we only have + or - symbols, the parentheses have to effects on the result.
+    That means that 8 combinations are straight foward.
+
+    When there is only * or / symbols, the parentheses have no effects on the result. 
+    8 more combinations are straight foward.
+
+    When there are one * or / symbol,
+*/
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -35,6 +47,488 @@ const rl = readline.createInterface({
 rl.question('Press enter to continue : ', (answer) => {
 
     console.log("The sum of all the minimal product-sum numbers for 2 ≤ k ≤ 12000 is", 
-    "ANSWER");
+    mainLoop());
     rl.close();
 });
+
+function mainLoop(){
+    let str;
+    let length = 0;
+    for(let a = 0; a < 6; ++a){
+        for(let b = 1; b < 7; ++b){
+            for(let c = 2; c < 8; ++c){       
+                for(let d = 3; d < 9; ++d){
+                    let r = makeEachPermutation(a,b,c,d);
+                    if (r > length){
+                        length = r;
+                        str = a.toString() + b.toString() + c.toString() + d.toString();
+                    }
+                }
+            }
+        }
+    }
+
+    return str;
+}
+function makeEachPermutation(a,b,c,d){
+    possibleNumbers = [];
+
+    callEachCombinations(a,b,c,d,possibleNumbers);
+    callEachCombinations(a,b,d,c,possibleNumbers);
+    callEachCombinations(a,c,b,d,possibleNumbers);
+    callEachCombinations(a,c,d,b,possibleNumbers);
+    callEachCombinations(a,d,b,c,possibleNumbers);
+    callEachCombinations(a,d,c,b,possibleNumbers);
+
+    callEachCombinations(b,a,c,d,possibleNumbers);
+    callEachCombinations(b,a,d,c,possibleNumbers);
+    callEachCombinations(b,c,a,d,possibleNumbers);
+    callEachCombinations(b,c,d,a,possibleNumbers);
+    callEachCombinations(b,d,a,c,possibleNumbers);
+    callEachCombinations(b,d,c,a,possibleNumbers);
+
+    callEachCombinations(c,a,b,d,possibleNumbers);
+    callEachCombinations(c,a,d,b,possibleNumbers);
+    callEachCombinations(c,b,a,d,possibleNumbers);
+    callEachCombinations(c,b,d,a,possibleNumbers);
+    callEachCombinations(c,d,a,b,possibleNumbers);
+    callEachCombinations(c,d,b,a,possibleNumbers);
+
+    callEachCombinations(d,a,b,c,possibleNumbers);
+    callEachCombinations(d,a,c,b,possibleNumbers);
+    callEachCombinations(d,b,a,c,possibleNumbers);
+    callEachCombinations(d,b,c,a,possibleNumbers);
+    callEachCombinations(d,c,a,b,possibleNumbers);
+    callEachCombinations(d,c,b,a,possibleNumbers);
+
+    return GetChainLength(possibleNumbers);
+
+}
+
+function callEachCombinations(a,b,c,d, possibleNumbers){
+    trivialCombinations(a,b,c,d, possibleNumbers);
+    OneOperatorParenthesesLeftCase(a,b,c,d, possibleNumbers);
+    OneOperatorParenthesesMiddleCase(a,b,c,d, possibleNumbers);
+    TwoOperatorCase(a,b,c,d,possibleNumbers);
+}
+
+function trivialCombinations(a,b,c,d, possibleNumbers){
+    r = a + b + c + d;
+    possibleNumbers[r] = r;
+
+    r = a + b + c - d;
+    if (r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a + b - c - d;
+    if (r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a - b + c + d;
+    if (r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a - b + c - d;
+    if (r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a - b - c + d;
+    if (r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a - b - c - d;
+    if (r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a * b * c * d;
+    possibleNumbers[r] = r;
+
+    r = a * b * (c / d);
+    if (Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+
+    r = a * (b / c) * d;
+    if (Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+
+    r = a * ((b / c) / d);
+    if (Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+
+    r = (a / b) * c * d;
+    if (Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+
+    r = (a / b) * (c / d);
+    if (Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+
+    r = ((a / b) / c) * d;
+    if (Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+
+    r = ((a / b) / c) / d;
+    if (Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+}
+
+function OneOperatorParenthesesLeftCase(a,b,c,d, possibleNumbers){
+    r = a * (b + c) + d;
+    possibleNumbers[r] = r; 
+    r = a * (b + c + d);
+    possibleNumbers[r] = r;
+
+    r = a * (b + c) - d;
+    if (r > 0){
+        possibleNumbers[r] = r; 
+    }
+
+    r = a * (b + c - d);
+    if (r > 0){
+        possibleNumbers[r] = r; 
+    }
+
+    r = a * (b - c) + d;
+    if (r > 0){
+        possibleNumbers[r] = r; 
+    }
+
+    r = a * (b - c + d);
+    if (r > 0){
+        possibleNumbers[r] = r;  
+    }
+
+    r = a * (b - c) - d;
+    if (r > 0){
+        possibleNumbers[r] = r;  
+    }
+
+    r = a * (b - c - d);
+    if (r > 0){
+        possibleNumbers[r] = r;  
+    }
+
+    r = (a / (b + c)) + d;
+    if (Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+
+    r = a / (b + c + d);
+    if (Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+
+    r = (a / (b + c)) - d;
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+
+    r = a / (b + c - d);
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+
+    r = (a / (b - c)) + d;
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+
+    r = a / (b - c + d);
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+
+    r = (a / (b - c)) - d;
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+
+    r = a / (b - c - d);
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+}
+
+function OneOperatorParenthesesMiddleCase(a,b,c,d, possibleNumbers){
+    r = a + (b * c) + d;
+    possibleNumbers[r] = r;
+
+    r = ((a + b) * c) + d;
+    possibleNumbers[r] = r;
+
+    r = ((a + b) * (c + d));
+    possibleNumbers[r] = r;
+
+    r = a + ((b * c) + d);
+    possibleNumbers[r] = r;
+
+
+    r = a + (b * c) - d;
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = ((a + b) * c) - d;
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = ((a + b) * (c - d));
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = a + ((b * c) - d);
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a - (b * c) + d;
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = ((a - b) * c) + d;
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = ((a - b) * (c + d));
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = a - ((b * c) + d);
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a - (b * c) - d;
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = ((a - b) * c) - d;
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = ((a - b) * (c - d));
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = a - ((b * c) - d);
+    if(r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a + (b / c) + d;
+    if(Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+    r = ((a + b) / c) + d;
+    if(Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+    r = ((a + b) / (c + d));
+    if(Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+    r = a + ((b / c) + d);
+    if(Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+
+    r = a + (b / c) - d;
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+    r = ((a + b) / c) - d;
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+    r = ((a + b) / (c - d));
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+    r = a + ((b / c) - d);
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+
+    r = a - (b / c) + d;
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+    r = ((a - b) / c) + d;
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+    r = ((a - b) / (c + d));
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+    r = a - ((b / c) + d);
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+
+    r = a - (b / c) - d;
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+    r = ((a - b) / c) - d;
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+    r = ((a - b) / (c - d));
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+    r = a - ((b / c) - d);
+    if (r > 0 && Number.isInteger(r)){
+        possibleNumbers[r] = r; 
+    }
+}
+
+function TwoOperatorCase(a,b,c,d, possibleNumbers){
+    r = (a * b * c) + d;
+    possibleNumbers[r] = r;
+
+    r = (a * b * c) - d;
+    if (r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = (a * (b / c)) + d;
+    if (Number.isInteger(r)){
+        possibleNumbers[r] = r;
+    }
+    r = (a * (b / c)) - d;
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = ((a / b) * c) + d;
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = ((a / b) * c) - d;
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = ((a / b) / c) + d;
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = ((a / b) / c) - d;
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+
+    r = (a * b * (c + d));
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = (a * b * (c - d));
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = (a * (b / (c + d)));
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = (a * (b / (c - d)));
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = ((a / b) * (c + d));
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = ((a / b) * (c - d));
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = ((a / b) / (c + d));
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = ((a / b) / (c - d));
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+
+    r = (a * ((b * c) + d));
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = (a * ((b * c) - d));
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a * ((b / c) + d);
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = a * ((b / c) - d);
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a / ((b * c) + d);
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = a / ((b * c) - d);
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = a / ((b / c) + d);
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = a / ((b / c) - d);
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = ((a / b) * (c + d));
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = (a / b) * (c - d);
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+
+    r = (a / b) / (c + d);
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+    r = (a / b) / (c - d);
+    if (Number.isInteger(r) && r > 0){
+        possibleNumbers[r] = r;
+    }
+}
+
+function GetChainLength(possibleNumbers){
+    let i = 1;
+    while (possibleNumbers[i] == i){
+        ++i;
+    }
+    return i - 1;
+}
