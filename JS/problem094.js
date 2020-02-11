9a79f65b15f3dfda349c899ecadd1f154fcbada7
Charles-David Lachance <charles-david.lachance@canada.ca>
2020-02-11 17:05:35 -0500

    Super duper slow but works

--- JS/problem094.js
+++ JS/problem094.js
@@ -22,6 +22,7 @@
 
     The area will be an integer if and only if ((a - 1)(3a + 1) and a + 1 = 0 mod 4)  or ((a + 1)(3a - 1) is a square and a - 1 = 0 mod 4).
 
+    We only need to compute for every fourth number since a = 1 mod 4.
 */
 
 const readline = require('readline');
@@ -35,20 +36,21 @@ rl.question('Press enter to continue : ', (answer) => {
 
     console.log("The sum of the perimeters of all almost equilateral triangles with integral", 
      "side lengths and area and whose perimeters do not exceed one billion is", 
-    GenerateHeronianTriangle(60));
+    LoopArea(1000000000));
+    //console.log(HeronsMethod(BigInt(25)));
     rl.close();
 });
 
-function LoopArea()
+function LoopArea(upperbound)
 {
     //1000000000
     let total = 0;
-    for (let side = 5; 3*side - 1 <= 1000000000; side += 2)
+    for (let side = 5; 3*side - 1 <= upperbound; side += 4)
     {  
-        if (side%100000 < 10)
-        {
-            console.log(3*side - 1);
-        }
+        // if (side%100000 < 10)
+        // {
+        //     console.log(3*side - 1);
+        // }
 
         total += IsHeronianTriangle(side);
     }
@@ -56,69 +58,38 @@ function LoopArea()
     return total;
 }
 
-function GenerateHeronianTriangle(upperbound){
-    let a = 0;
-    let b = 0;
-    let c = 0;
-    let m = 0;
-    let total_perimeter = 0;
-    let perimeter = 0;
-    while(perimeter < upperbound){
-        m++;
-        for(let n = 1; n <= m; n++){
-            for(let k = 1; k**2 < n*m; k++){
-                if(gcd(m,gcd(n,k)) == 1){
-                    a = n*(m**2 + k**2);
-                    b = m*(n**2 + k**2);
-                    c = (m + n)*(m*n - k**2);
-                    console.log(a,b,c);
-                    perimeter = a + b + c;
-                }
-                if(AlmostEquilateral(a,b,c)){
-                    total_perimeter += perimeter;
-                }
-            }
-        }
-    }
-
-    return total_perimeter;
-}
-
-function AlmostEquilateral(a,b,c){
-    if(a == b && Math.abs(a - c) == 1){
-        return true;
-    }
-    if(a == c && Math.abs(a - b) == 1){
-        return true;
-    }
-    if(b == c && Math.abs(b - a) == 1){
-        return true;
-    }
-    return false;
-}
-function gcd(x,y){
-    while(y) {
-        var t = y;
-        y = x % y;
-        x = t;
-      }
-      return x;
-}
 
 function IsHeronianTriangle(side)
 {
     let total_perimeter = 0;
-    console.log((side-1)*(3*side + 1));
-    let test_plus1 = Math.sqrt((side - 1)*(3*side + 1));
-    let test_minus1 = Math.sqrt((side + 1)*(3*side - 1));
 
-    if(Number.isInteger(test_plus1) && (side + 1)*test_plus1 %4 == 0){
+
+    let test_plus1 = BigInt((side - 1))*BigInt((3*side + 1));
+    let test_minus1 = BigInt((side + 1))*BigInt((3*side - 1));
+
+    if(HeronsMethod(test_plus1)){
         total_perimeter += 3*side + 1;
     }
 
 
-    if(Number.isInteger(test_minus1)   && (side - 1)*test_minus1 %4 == 0){
+    if(HeronsMethod(test_minus1)){
         total_perimeter += 3*side - 1;
     }
     return total_perimeter;
+}
+
+function HeronsMethod(potentialsquare){
+    let x_0 = potentialsquare/BigInt(2);
+    let x_1 = (x_0 + potentialsquare/x_0)/BigInt(2);
+    let potentialroot = [x_0];
+    while(!potentialroot.includes(x_1)){
+        // console.log(potentialsquare, x_0, x_1, potentialroot);
+        x_0 = x_1;
+        x_1  = (x_0 + potentialsquare/x_0)/BigInt(2);
+        potentialroot.push(x_0);
+    }
+    if (x_1**BigInt(2) == potentialsquare){
+        return true;
+    }
+    return false;
 }
