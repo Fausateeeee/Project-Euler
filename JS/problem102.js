60b78e3b730e4ce29e4292fe5b729f1065e354dc
Charles-David Lachance <charles-david.lachance@canada.ca>
2020-02-14 17:46:49 -0500

    Solved

--- JS/problem102.js
+++ JS/problem102.js
@@ -29,7 +29,35 @@ const rl = readline.createInterface({
 
 rl.question('Press enter to continue : ', (answer) => {
 
-    console.log("The number of triangles which contains the origin is", "ANSWER");
+    console.log("The number of triangles which contains the origin is", ReadFile());
     rl.close();
 });
 
+
+function ReadFile()
+{
+    let total = 0;
+    let allText = fs.readFileSync("../Additional-Files/p102_triangles.txt", 'utf8');
+    let allTriangles = allText.split("\n");
+
+    for (let coordinates of allTriangles)
+    {
+        let points = coordinates.split(",");
+        let A = [Number.parseInt(points[0]), Number.parseInt(points[1])];
+        let B = [Number.parseInt(points[2]), Number.parseInt(points[3])];
+        let C = [Number.parseInt(points[4]), Number.parseInt(points[5])];
+        
+        if (BarycentricCoordinates(A,B,C)){
+            ++total;
+        }
+    }
+    return total;
+}
+
+function BarycentricCoordinates(A,B,C){
+    let lambda1 = ((B[1] - C[1])*(-1*C[0]) + (C[0] - B[0])*(-1*C[1]))/((B[1] - C[1])*(A[0] - C[0]) + (C[0] - B[0])*(A[1] - C[1]));
+    let lambda2 = ((C[1] - A[1])*(-1*C[0]) + (A[0] - C[0])*(-1*C[1]))/((B[1] - C[1])*(A[0] - C[0]) + (C[0] - B[0])*(A[1] - C[1]));
+    let lambda3 = 1 - lambda1 - lambda2;
+    console.log(lambda1, lambda2, lambda3);
+    return lambda1 > 0 && lambda2 > 0 && lambda3 > 0;
+}
