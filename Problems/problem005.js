e17475ace38abbdac0f9bd2ff7541c36291f788c
Charles-David Lachance <charles-david.lachance@fausa.tech>
2020-08-08 22:21:17 -0400

    Problem 5 updated

--- Problems/problem005.js
+++ Problems/problem005.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 6 */
+const { performance } = require('perf_hooks')
 /*
 
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
@@ -6,56 +6,32 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 */
-
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('Enter a natural number ', (answer) => {
-
-    const parsed = parseInt(answer);
-
-    if (isNaN(parsed)) {
-        console.log("Please, enter a natural number next time <3");
-        rl.close();
-    }
-
-    if (parsed < 1) {
-        console.log("Enter a number greater than 0 next time");
-        rl.close();
-    }
-    
-    let lcmf = 1;
-    for (let i = 1; i <= answer; i++)
-    {
-        lcmf = lcm(lcmf, i);
-    }
-    
-    console.log("The smallest positive number that is evenly divisible by all of the number from 1 to", parsed, " is : ", lcmf);
-    rl.close();
-});
-
-function lcm(number1, number2)
-{
-    return number1*number2/gcd(number1,number2);
+function p005 (n) {
+  const t0 = performance.now()
+  let lcmf = 1
+  for (let i = 1; i <= n; i++) {
+    lcmf = lcm(lcmf, i)
+  }
+  const t1 = performance.now()
+  return { answer: lcmf, time: t1 - t0 }
 }
 
-function gcd(number1, number2)
-{
-    if (number1 < number2)
-    {
-        return gcd(number2, number1);
-    }
-
-    while (number2 > 0)
-    {
-        let rest = number1%number2;
-        number1 = number2;
-        number2 = rest;
-    }
-
-    return number1;
+function lcm (number1, number2) {
+  return number1 * number2 / gcd(number1, number2)
 }
+
+function gcd (number1, number2) {
+  if (number1 < number2) {
+    return gcd(number2, number1)
+  }
+
+  while (number2 > 0) {
+    const rest = number1 % number2
+    number1 = number2
+    number2 = rest
+  }
+
+  return number1
+}
+
+module.exports = p005
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const p001 = require('./Problems/problem001')
 const p002 = require('./Problems/problem002')
 const p003 = require('./Problems/problem003')
+const p004 = require('./Problems/problem004')
 
 const a001 = p001(1000)
 console.log(a001)
@@ -10,3 +11,6 @@ console.log(a002)
 
 const a003 = p003(600851475143)
 console.log(a003)
+
+const a004 = p004(800000)
+console.log(a004)
