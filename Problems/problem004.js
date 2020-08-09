8a18aa8d74468df68caf2d49eeaffa104b8b464a
Charles-David Lachance <charles-david.lachance@fausa.tech>
2020-08-08 22:15:21 -0400

    Worked with hackerrank constraint

--- Problems/problem004.js
+++ Problems/problem004.js
@@ -1,3 +1,4 @@
+const { performance } = require('perf_hooks')
 /*
 
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
@@ -5,41 +6,23 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 
 */
-const readline = require('readline')
+/* Solving the variation of this problem of finding the largest palindrome made from the product of two 3-digits numbers which is less than N. */
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-rl.question('Enter a natural number ', (answer) => {
-  const parsed = parseInt(answer)
-
-  if (isNaN(parsed)) {
-    console.log('Please, enter a natural number next time <3')
-    return 0
-  }
-
-  if (parsed < 1) {
-    console.log('Enter a number greater than 0 next time')
-    rl.close()
-  }
-
-  const inf = Math.pow(10, parsed - 1) // parsed = 1 => inf = 1, parsed = 2 => inf = 10, etc...
-  const sup = Math.pow(10, parsed) // parsed = 1 => sup = 10, parsed = 2 => sup = 100, etc...
-
+function p004 (n) {
+  const t0 = performance.now()
+  const inf = Math.pow(10, 3 - 1) // parsed = 1 => inf = 1, parsed = 2 => inf = 10, etc...
+  const sup = Math.pow(10, 3) // parsed = 1 => sup = 10, parsed = 2 => sup = 100, etc...
   let maxPal = 0
-
   for (let i = inf; i < sup; i++) {
     for (let j = i; j < sup; j++) {
-      if (isPalindrome(i * j) && i * j > maxPal) {
+      if (i * j < n && isPalindrome(i * j) && i * j > maxPal) {
         maxPal = i * j
       }
     }
   }
-  console.log('The largest palindrome made from the product of a', parsed, '-digit number is : ', maxPal)
-  rl.close()
-})
+  const t1 = performance.now()
+  return { answer: maxPal, time: t1 - t0 }
+}
 
 function isPalindrome (number) {
   const arr = String(number).split('')
@@ -47,9 +30,11 @@ function isPalindrome (number) {
   let j = arr.length - 1
 
   for (let i = 0; i < j; i++, j--) {
-    if (arr[i] != arr[j]) {
+    if (arr[i] !== arr[j]) {
       return false
     }
   }
   return true
 }
+
+module.exports = p004
