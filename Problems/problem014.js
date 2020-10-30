13dd94a98bf5b2f1fc7f2d4b58c47dc0adbb516a
Charles-David Lachance <charles-david.lachance@fausa.tech>
2020-10-30 02:08:33 -0400

    Changes to the way the sequence is computed + small fix

--- Problems/problem014.js
+++ Problems/problem014.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 6 */
+const { performance } = require('perf_hooks')
 /*
 
 The following iterative sequence is defined for the set of positive integers:
@@ -17,74 +17,39 @@ NOTE: Once the chain starts the terms are allowed to go above one million.
 
 */
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('Enter a natural number ', (answer) => {
-  
-    const parsed = parseInt(answer);
-    var collatzValues = [];
-
-    if (isNaN(parsed)) {
-        console.log("Please, enter a natural number next time <3");
-        rl.close();
-    }
-
-    if (parsed < 1) {
-        console.log("Enter a number greater than 0 next time");
-        rl.close();
-    
-    }
-    let r_collatz = Collatz(parsed);
-
-    let max = r_collatz.reduce((a,b) => Math.max(a,b));
-
-    console.log("The number with the longuest Collatz chain under", parsed, "is", r_collatz.indexOf(max), "with length", max);
-    rl.close();
-});
-
-//Collatz will return the starting number and the length of the chain
-
-function Collatz(nbr)
-{
-    let values = [true, true];
-    let length = [0,1];
-
-    for (let i = 2; i < nbr; i++)
-    {
-        let number = i;
-        let length_l = 1;
-        while(number > 1)
-        {
-            if (values[number])
-            {
-               
-                values[i] = true;
-                length[i] = length[number] + length_l;
-                number=1;
-            }
-
-            else if (number%2 == 0)
-            {
-                length_l++;
-                number /= 2;
-                if (number == 1)
-                {
-                    values[i] = true;
-                    length[i] = length_l;
-                }         
-            }
-            else
-            {
-                length_l++;
-                number = 3*number + 1;
-            }
-        }
-        
-    }
-    return length;
+function p014 (n) {
+  const t0 = performance.now()
+  const answer = Collatz(n)
+  const t1 = performance.now()
+  return { answer: answer, time: t1 - t0 }
 }
+
+function Collatz (upperBound) {
+  var chain = [0, 1, 2]
+  var max = [2, 2]
+  for (let currentValue = 3; currentValue < upperBound; ++currentValue) {
+    if (currentValue % 2 === 0) {
+      chain[currentValue] = chain[currentValue / 2] + 1
+    } else {
+      let length = 1
+      let collatzNumber = 3 * currentValue + 1
+      do {
+        if (collatzNumber % 2 === 0) {
+          ++length
+          collatzNumber /= 2
+        } else {
+          ++length
+          collatzNumber = 3 * collatzNumber + 1
+        }
+      }
+      while (collatzNumber > currentValue)
+      chain[currentValue] = chain[collatzNumber] + length
+      if (chain[currentValue] >= max[1]) {
+        max = [currentValue, chain[currentValue]]
+      }
+    }
+  }
+  return max[0]
+}
+
+module.exports = p014
