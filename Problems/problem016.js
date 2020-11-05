ad53b51312385aee6eb85137975fc169f660f694
Charles-David Lachance <charles-david.lachance@fausa.tech>
2020-11-05 18:52:47 -0500

    Updated to use module

--- Problems/problem016.js
+++ Problems/problem016.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 6 */
+const { performance } = require('perf_hooks')
 /*
 
 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
@@ -7,36 +7,12 @@ What is the sum of the digits of the number 2^1000?
 
 */
 
+function p016 (n) {
+  const t0 = performance.now()
+  const answer = (BigInt(2) ** BigInt(n)).toString().split('').reduce((acc, val) => { return acc + Number.parseInt(val) }, 0)
 
-/*
-    I will need an implementation of BigInt
-    Run npm install big-integer 
-*/
-const bigInt = require("big-integer");
-const readline = require('readline');
+  const t1 = performance.now()
+  return { answer: answer, time: t1 - t0 }
+};
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('Enter a natural number ', (answer) => {
-
-    const parsed = parseInt(answer);
-
-    if (isNaN(parsed)) {
-        console.log("Please, enter a natural number next time <3");
-        rl.close();
-    }
-
-    if (parsed < 1) {
-        console.log("Enter a number greater than 0 next time");
-        rl.close();
-
-    }
-
-    let bi = bigInt(2).pow(parsed).toArray(10);
-
-    console.log("The sum of the digits of 2 to the power of", parsed, "is :", bi.value.reduce((a,b) => a+b));
-    rl.close();
-});
+module.exports = p016
