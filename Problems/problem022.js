214ea1af44122037aaf7165e7d0f172e1e646044
Charles-David Lachance <charles-david.lachance@fausa.tech>
2020-12-29 18:05:01 -0500

    Updated to a node module

--- Problems/problem022.js
+++ Problems/problem022.js
@@ -1,55 +1,80 @@
-/*jshint esversion: 6 */
+const { performance } = require('perf_hooks')
+const fs = require('fs')
 /*
 
-Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, 
-begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by 
+Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names,
+begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by
 its alphabetical position in the list to obtain a name score.
 
-For example, when the list is sorted into alphabetical order, COLIN, 
+For example, when the list is sorted into alphabetical order, COLIN,
 which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
 
 What is the total of all the name scores in the file?
 
-
 */
 
-const readline = require('readline');
-const fs = require('fs');
+const SCORE = {
+  A: 1,
+  B: 2,
+  C: 3,
+  D: 4,
+  E: 5,
+  F: 6,
+  G: 7,
+  H: 8,
+  I: 9,
+  J: 10,
+  K: 11,
+  L: 12,
+  M: 13,
+  N: 14,
+  O: 15,
+  P: 16,
+  Q: 17,
+  R: 18,
+  S: 19,
+  T: 20,
+  U: 21,
+  V: 22,
+  W: 23,
+  X: 24,
+  Y: 25,
+  Z: 26
+}
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function p022 (path) {
+  const t0 = performance.now()
+  const names = ReadFile(path)
+  const answer = ComputeScore(names)
+  const t1 = performance.now()
+  return { answer: answer, time: t1 - t0 }
+}
 
-const SCORE = {A : 1, B : 2, C : 3, D : 4, E : 5, F : 6, G : 7, H : 8, I : 9, J : 10, K : 11, L : 12, M : 13, N : 14, O : 15,
-P : 16, Q : 17, R : 18, S : 19, T : 20, U : 21, V : 22, W : 23, X : 24, Y : 25, Z : 26};
+function ReadFile (path) {
+  // Read the file
+  let allText = fs.readFileSync(path, 'utf8')
+  // Remove  every " " from the file
+  const regex = /"/g
+  allText = allText.replace(regex, '')
 
-rl.question('Write the path to a valid input file ', (answer) => {
-    
-    //Read the file
-    let allText = fs.readFileSync(answer, 'utf8');
-    //Remove  every " " from the file
-    let regex = /"/g;
-    allText = allText.replace(regex,"");
+  // Split the names into an array
+  const arrNames = allText.split(',')
 
-    //Split the names into an array
-    let arrNames = allText.split(",");
+  // Sort the array using default sort
+  return arrNames.sort()
+}
 
-    //Sort the array using default sort
-    arrNames.sort();
-    
-    let arrScores = [];
-    for (let i = 0; i < arrNames.length; i++)
-    {
-        let currentScore = 0;
-        for(let letter of arrNames[i])
-        {
-            currentScore += SCORE[letter];
-        }
-        arrScores.push((i+1) * currentScore);
+function ComputeScore (arrNames) {
+  let arrScores = 0
+  for (let i = 0; i < arrNames.length; i++) {
+    let currentScore = 0
+    for (const letter of arrNames[i]) {
+      currentScore += SCORE[letter]
     }
+    arrScores += (i + 1) * currentScore
+  }
 
-    console.log("The total score of every name scrores in the file is :", arrScores.reduce((a,b) => {return a+b;}));
-    rl.close();
-});
+  return arrScores
+}
 
+module.exports = p022
--- Problems/problem023.js
+++ Problems/problem023.js
@@ -1,101 +1,69 @@
-/*jshint esversion: 6 */
+const { performance } = require('perf_hooks')
 /*
 
-A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. 
+A perfect number is a number for which the sum of its proper divisors is exactly equal to the number.
 For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
 
 A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
 
-As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, 
-the smallest number that can be written as the sum of two abundant numbers is 24. 
-By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. 
-However, this upper limit cannot be reduced any further by analysis even though it is known that the 
+As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16,
+the smallest number that can be written as the sum of two abundant numbers is 24.
+By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers.
+However, this upper limit cannot be reduced any further by analysis even though it is known that the
 greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
 
 Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
 
 */
 
-const readline = require('readline');
+function p023 (n) {
+  const t0 = performance.now()
+  const abundant = GetAbundantNumber(n)
+  const abundantSum = GetSumOfEveryTwoAbundant(abundant, n)
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-
-rl.question('Write a natural number ', (answer) => {
-    
-    let parsed = parseInt(answer);
-
-    if (isNaN(parsed)) {
-        console.log("Please, enter a natural number next time <3");
-        rl.close();
+  let answer = 0
+  for (let i = 1; i <= n; i++) {
+    if (abundantSum.indexOf(i) === -1) {
+      answer += i
     }
-
-    if (parsed <= 0) {
-        console.log("Enter a number greater than 0 next time");
-        rl.close();
-    }
-
-    let abundant = getAbundantNumber(parsed);
-    let abundantSum = getSumOfEveryTwoAbundant(abundant, parsed);
-    
-    let nonAbundant = 0;
-    for (let i = 1; i <= parsed; i++)
-    {
-        if (abundantSum.indexOf(i) == -1)
-        {
-            nonAbundant += i;
-        }
-    }
-    console.log("The sum of positive integers smaller than", parsed, 
-    "that cannot be written as the sum of two abundant numbers is :", nonAbundant);
-    rl.close();
-});
-
-function reduceFactor(number)
-{
-    factor = [1];
-    for (let i = 2; i <= Math.ceil(number/2); i++)
-    {
-        if (number%i == 0)
-        {
-            factor.push(i);
-        }
-    }
-
-    return factor.reduce((a,b) => {return a+b;});
+  }
+  const t1 = performance.now()
+  return { answer: answer, time: t1 - t0 }
 }
 
-function getAbundantNumber(number)
-{
-    let abundant = [];
-    for (let i = 2; i <= number; i++)
-    {
-        let divisorSum = reduceFactor(i);
-        if (divisorSum > i)
-        {
-            abundant.push(i);
-        }
+function reduceFactor (number) {
+  const factor = [1]
+  for (let i = 2; i <= Math.ceil(number / 2); i++) {
+    if (number % i === 0) {
+      factor.push(i)
     }
+  }
 
-    return abundant;
+  return factor.reduce((a, b) => { return a + b })
 }
 
-function getSumOfEveryTwoAbundant(abundant)
-{
-    let abundantSum = [];
-
-    for (let i = 0; i < abundant.length; i++)
-    {
-        for (let j = i; j < abundant.length; j++)
-        {
-
-            abundantSum.push(abundant[i] + abundant[j]);
-
-        }
+function GetAbundantNumber (number) {
+  const abundant = []
+  for (let i = 2; i <= number; i++) {
+    const divisorSum = reduceFactor(i)
+    if (divisorSum > i) {
+      abundant.push(i)
     }
+  }
 
-    return abundantSum;
+  return abundant
 }
+
+function GetSumOfEveryTwoAbundant (abundant) {
+  const abundantSum = []
+
+  for (let i = 0; i < abundant.length; i++) {
+    for (let j = i; j < abundant.length; j++) {
+      abundantSum.push(abundant[i] + abundant[j])
+    }
+  }
+
+  return abundantSum
+}
+
+module.exports = p023
