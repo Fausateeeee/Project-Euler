a8bc5e714e5a8c22a207946a0c27f6d904b78779
Charles-David Lachance <charles-david.lachance@fausa.tech>
2020-12-30 10:58:11 -0500

    Updated to node module but still slow

--- Problems/problem026.js
+++ Problems/problem026.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 6 */
+const { performance } = require('perf_hooks')
 /*
 
 A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
@@ -11,99 +11,66 @@ A unit fraction contains 1 in the numerator. The decimal representation of the u
     1/7	= 	0.(142857)
     1/8	= 	0.125
     1/9	= 	0.(1)
-    1/10	= 	0.1 
+    1/10	= 	0.1
 
 Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
 
 Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
 
-
 */
 
 /*
-    I know that if the denominator has a prime factorisation of the form (2^n)(5^m), it has a finite cycle. 
+    I know that if the denominator has a prime factorisation of the form (2^n)(5^m), it has a finite cycle.
     From group theory, we try to compute the length of a cycle, or more so the order of the cyclic group.
 */
-const readline = require('readline');
-const bigInt = require("big-integer");
+function p026 (n) {
+  const t0 = performance.now()
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+  const findCycle = {}
+  let answer = 1
 
-rl.question('Enter a natural number ', (answer) => {
-
-    const parsed = parseInt(answer);
-
-    if (isNaN(parsed)) {
-        console.log("Please, enter a natural number next time <3");
-        rl.close();
+  for (let i = 1; i < n; ++i) {
+    console.log(i)
+    findCycle[i] = findCycleLength(i)
+    if (findCycle[i] > findCycle[answer]) {
+      answer = i
     }
+  }
 
-    if (parsed < 1) {
-        console.log("Enter a number greater than 0 next time");
-        rl.close();
-    }
-
-    let findCycle = {};
-    let max = 1;
-    for (let i = 1; i < answer; ++i)
-    {
-        findCycle[i] = findCycleLength(i);
-        if (findCycle[i] > findCycle[max])
-        {
-            max = i;
-        }
-    }
-
-    //console.log(findCycleLength(17));
-    console.log(findCycle);
-    console.log("The fraction where the denominator is smaller than", parsed, " that has the longuest recurring cycle is :", max);
-    rl.close();
-});
-
-function findCycleLength(denominator)
-{
-    if (hasNoCycle(denominator) || denominator == 1)
-    {   
-        return 0; //No cycle
-    }
-
-    else
-    {
-        let i = 1;
-        while(true)
-        {
-
-            for (let j = i - 1; j >= 0; j--)
-            {
-                if (bigInt(10).pow(i).minus(bigInt(10).pow(j)).mod(denominator) == 0)
-                {
-                    return i - j;
-                }
-            }
-            i++;
-
-        }
-    }
+  const t1 = performance.now()
+  return { answer: answer, time: t1 - t0 }
 }
 
-function hasNoCycle(number)
-{
-    let temp = number;
-    while (temp%2 == 0)
-    {
-        temp /= 2;
+function findCycleLength (denominator) {
+  if (hasNoCycle(denominator) || denominator === 1) {
+    return 0 // No cycle
+  } else {
+    let i = 1
+    while (true) {
+      for (let j = i - 1; j >= 0; --j) {
+        // console.log(BigInt(10) ** BigInt(i), BigInt(10) ** BigInt(j), (BigInt(10) ** BigInt(i) - (BigInt(10) ** BigInt(j))) % BigInt(denominator), (BigInt(10) ** BigInt(i) - (BigInt(10) ** BigInt(j))) % BigInt(denominator) === 0n)
+        if ((BigInt(10) ** BigInt(i) - (BigInt(10) ** BigInt(j))) % BigInt(denominator) === BigInt(0)) {
+          return i - j
+        }
+      }
+      ++i
     }
-    while (temp%5 == 0)
-    {
-        temp /= 5;
-    }
-    
-    if (temp == 1)
-    {
-        return true;
-    }
-    return false;
+  }
 }
+
+function hasNoCycle (number) {
+  let temp = number
+  while (temp % 2 === 0) {
+    temp /= 2
+  }
+  while (temp % 5 === 0) {
+    temp /= 5
+  }
+
+  if (temp === 1) {
+    return true
+  }
+  return false
+}
+
+module.exports = p026
