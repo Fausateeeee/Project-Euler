683eb1bf13fc4b679abc2408e073f1c12c8e0a32
Charles-David Lachance <charles-david.lachance@fausa.tech>
2021-01-05 15:17:24 -0500

    It is finally done! :)

--- Problems/problem098.js
+++ Problems/problem098.js
@@ -1,14 +1,15 @@
-/*jshint esversion: 7 */
+const { performance } = require('perf_hooks')
+const fs = require('fs')
 /*
 
-    By replacing each of the letters in the word CARE with 1, 2, 9, and 6 respectively, 
-    we form a square number: 1296 = 36^2. What is remarkable is that, 
-    by using the same digital substitutions, the anagram, RACE, also forms a square number: 9216 = 96^2. 
-    We shall call CARE (and RACE) a square anagram word pair and specify further that leading zeroes are not permitted, 
+    By replacing each of the letters in the word CARE with 1, 2, 9, and 6 respectively,
+    we form a square number: 1296 = 36^2. What is remarkable is that,
+    by using the same digital substitutions, the anagram, RACE, also forms a square number: 9216 = 96^2.
+    We shall call CARE (and RACE) a square anagram word pair and specify further that leading zeroes are not permitted,
     neither may a different letter have the same digital value as another letter.
 
-    Using words.txt (right click and 'Save Link/Target As...'), 
-    a 16K text file containing nearly two-thousand common English words, 
+    Using words.txt (right click and 'Save Link/Target As...'),
+    a 16K text file containing nearly two-thousand common English words,
     find all the square anagram word pairs (a palindromic word is NOT considered to be an anagram of itself).
 
     What is the largest square number formed by any member of such a pair?
@@ -17,125 +18,186 @@
 
 */
 
-const readline = require('readline');
-const fs = require('fs');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function p098 (path) {
+  const t0 = performance.now()
 
-rl.question('Press enter to continue : ', (answer) => {
+  const wordDict = ReadFile(path)
+  const squares = new Squares()
+  const answer = FindPairs(wordDict, squares)
 
-    console.log("The sum of all the minimal product-sum numbers for 2 ≤ k ≤ 12000 is", 
-    p98());
-    rl.close();
-});
-
-function p98(){
-    let wordDict = ReadFile();
-    let anagramDict = GetAnagramPair(wordDict);
-    let squares = GenerateSquares(anagramDict);
-    return 0;
+  const t1 = performance.now()
+  return { answer: answer, time: t1 - t0 }
 }
 
-function ReadFile(){
-    let allText = fs.readFileSync("../Additional-Files/p098_words.txt", 'utf8');
+function ReadFile (path) {
+  const allText = fs.readFileSync(path, 'utf8')
 
-    let allWords = allText.split(",");
+  const allWords = allText.split(',')
 
-    let wordDict = {};
-    for (let word of allWords){
-        split = word.split("\"");
-        wordObj = new Word(split[1]);
-        if(wordObj.anagram in wordDict){
-            wordDict[wordObj.anagram].push(wordObj);
+  const wordDict = {}
+  for (const word of allWords) {
+    const split = word.split('"')
+    const wordObj = new Word(split[1])
+    if (Object.keys(wordDict).includes(wordObj.anagram)) {
+      wordDict[wordObj.anagram].push(wordObj)
+    } else {
+      wordDict[wordObj.anagram] = [wordObj]
+    }
+  }
+  for (const key of Object.keys(wordDict)) {
+    if (wordDict[key].length === 1) {
+      delete wordDict[key]
+    }
+  }
+
+  return wordDict
+}
+
+function FindPairs (wordsDict, squares) {
+  let max = 0
+  for (const key in wordsDict) {
+    const wordsPairs = wordsDict[key]
+    const potentialsSquares = squares.GetSquares(wordsPairs[0].word.length)
+    for (let i = 0; i < wordsPairs.length - 1; ++i) {
+      const word1 = wordsPairs[i]
+      for (let j = i + 1; j < wordsPairs.length; ++j) {
+        const word2 = wordsPairs[j]
+        for (let k = potentialsSquares.length - 1; k >= 0; --k) {
+          const patron = CurrentWordFitsSquare(word2, potentialsSquares[k])
+          const potentialSquareAnagram = IsSquareAnagram(patron, word1, potentialsSquares)
+          if (potentialSquareAnagram !== false) {
+            max = Math.max(potentialSquareAnagram, potentialsSquares[k], max)
+          }
         }
-        else{
-            wordDict[wordObj.anagram] = [wordObj];
-        }
+      }
     }
-
-    return wordDict;
+  }
+  return max
 }
 
-function GetAnagramPair(wordDict){
-    anagramPair = {};
-    for (let key of Object.keys(wordDict)){
-        if(wordDict[key].length > 1){
-            anagramPair[key] = wordDict[key];
-        }
+function CurrentWordFitsSquare (word, square) {
+  const squareWord = square.toString()
+  const dict = {}
+  let constructedSquare = ''
+  for (const letterIndex in word.word) {
+    const letter = word.word[letterIndex]
+    if (letter in Object.keys(dict)) {
+      constructedSquare += dict[letter]
+    } else {
+      dict[letter] = squareWord[letterIndex]
+      constructedSquare += dict[letter]
     }
-
-    return anagramPair;
+  }
+  if (Object.values(dict).unique().length < word.anagram.length) {
+    return false
+  }
+  if (constructedSquare === squareWord) {
+    return dict
+  }
+  return false
 }
 
-function GenerateSquares(anagramDict){
-    let SquareAnagrams = [];
-    for (let key of Object.keys(anagramDict)){
-        for (let i = 0; i < anagramDict[key].length - 1; ++i){
-            for (let j = i+1; j < anagramDict[key].length; ++j){
-                if(new SquareAnagram(anagramDict[key][i], anagramDict[key][j]).isSquareAnagram()){
+function IsSquareAnagram (patron, word, squares) {
+  if (patron === false) {
+    return false
+  }
+  let constructedNumber = ''
+  for (const letter of word.word) {
+    constructedNumber += patron[letter]
+  }
+  const num = Number.parseInt(constructedNumber)
 
-                }
-            }
-        }
-
-    }
-}
-class Word{
-    constructor(word){
-        this.word = word;
-        this.uniqueletters = word.split("").unique();
-        this.anagram = this.word.split("").sort().reduce((a,b) => {return a + b;});
-    }
-}
-class Square{
-    constructor(length){
-        this.squares = [1];
-        this.length = length;
-        let i = 2;
-        while (this.squares[this.squares.length - 1].toString().length <= length){
-            this.squares.push(i**2);
-            ++i;
-        }
-    }
+  if (squares.includes(num)) {
+    return num
+  }
+  return false
 }
 
-class CombinationPermutation{
-    constructor(){
-        this.numbers = [0,1,2,3,4,5,6,7,8,9];
-
-    }
-
-    GetCombination(length){
-
-    }
-
-    GetPermutation(Combination){
-        
-    }
-}
-class SquareAnagram{
-    constructor(firstword, secondword){
-        this.w1 = firstword;
-        this.w2 = secondword;
-    }
-
-    isSquareAnagram() {
-        return false;
-        
-    }
+class Word {
+  constructor (word) {
+    this.word = word
+    this.uniqueletters = word.split('').unique().sort().reduce((a, b) => { return a + b })
+    this.anagram = this.word.split('').sort().reduce((a, b) => { return a + b })
+  }
 }
 
-Array.prototype.unique = function() {
-    var a = this.concat();
-    for(var i=0; i<a.length; ++i) {
-        for(var j=i+1; j<a.length; ++j) {
-            if(a[i] === a[j])
-                a.splice(j--, 1);
-        }
+class Squares {
+  constructor () {
+    this.s1 = []
+    this.s2 = []
+    this.s3 = []
+    this.s4 = []
+    this.s5 = []
+    this.s6 = []
+    this.s7 = []
+    this.s8 = []
+    this.s9 = []
+    this.GenerateSquares()
+  }
+
+  GenerateSquares () {
+    let b = 1
+    let s = 1
+    while ((s = b ** 2) < 1000000000) {
+      this.StoreSquare(s)
+      ++b
     }
+  }
 
-    return a;
-};
+  StoreSquare (s) {
+    if (s < 10) {
+      this.s1.push(s)
+    } else if (s < 100) {
+      this.s2.push(s)
+    } else if (s < 1000) {
+      this.s3.push(s)
+    } else if (s < 10000) {
+      this.s4.push(s)
+    } else if (s < 100000) {
+      this.s5.push(s)
+    } else if (s < 1000000) {
+      this.s6.push(s)
+    } else if (s < 10000000) {
+      this.s7.push(s)
+    } else if (s < 100000000) {
+      this.s8.push(s)
+    } else if (s < 1000000000) {
+      this.s9.push(s)
+    }
+  }
 
+  GetSquares (l) {
+    switch (l) {
+      case 1:
+        return this.s1
+      case 2:
+        return this.s2
+      case 3:
+        return this.s3
+      case 4:
+        return this.s4
+      case 5:
+        return this.s5
+      case 6:
+        return this.s6
+      case 7:
+        return this.s7
+      case 8:
+        return this.s8
+      case 9:
+        return this.s9
+    }
+  }
+}
+
+Array.prototype.unique = function () {
+  var a = this.concat()
+  for (var i = 0; i < a.length; ++i) {
+    for (var j = i + 1; j < a.length; ++j) {
+      if (a[i] === a[j]) { a.splice(j--, 1) }
+    }
+  }
+
+  return a
+}
+module.exports = p098
